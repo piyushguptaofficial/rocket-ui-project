e042c70e5db39311b07ee42c754774a5e731dd7b
Piyush Gupta <100441111+piyushguptaofficial@users.noreply.github.com>
2025-06-29 15:40:46 +0530

    Delete src/components/Skeleton.jsx

--- src/components/Skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skeleton = ({ className = '' }) => {
-  return (
-    <div className={`bg-gray-700 rounded animate-pulse ${className}`} />
-  );
-};
-
-export default Skeleton;
