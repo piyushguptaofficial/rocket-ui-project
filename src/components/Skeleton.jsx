9c2cc6cba10f384d7183dc6ca2c9862c08280886
Piyush Gupta <100441111+piyushguptaofficial@users.noreply.github.com>
2025-06-29 15:02:07 +0530

    Create Skeleton.jsx

---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,10 @@
+// src/components/Skeleton.jsx
+import React from 'react';
+
+const Skeleton = ({ className = '', rounded = 'rounded-md' }) => {
+  return (
+    <div className={`bg-gray-700 animate-pulse ${rounded} ${className}`} />
+  );
+};
+
+export default Skeleton;
